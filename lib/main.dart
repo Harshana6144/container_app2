cfbb358dfb56dde42f04054239bc592f53b6b4ab
Harshana Prasad <harshana7096@gmail.com>
2024-05-11 15:10:44 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home:SafeArea(
+      child:Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.greenAccent
+              ),
+            )
+          ],
+          
+        ),
+      ),
+    ),
+  );
+}
+
+}
